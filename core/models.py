c323e5bc43c6f5c06d55784dd71f810f93871e49
rakib06 <six.rakib@gmail.com>
2020-05-05 05:12:36 +0600

    models are added

--- core/models.py
+++ core/models.py
@@ -4,20 +4,26 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
+    price = models.FloatField(max_length=100)
 
     def __str__(self):
         return self.title
 
 
 class OrderItem(models.Model):
-    pass
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 
 class Order(models.Model):
-    pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
