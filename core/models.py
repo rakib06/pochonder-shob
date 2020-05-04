2727449e9cabac53ad12c040d3fd512c0f4524db
rakib06 <six.rakib@gmail.com>
2020-05-05 04:38:49 +0600

    initial commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class OrderItem(models.Model):
+    pass
+
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    pass
+
+    def __str__(self):
+        return self.title
