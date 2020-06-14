fe0d39c114acd9cd635e9e346d8e411812786646
rakib06 <six.rakib@gmail.com>
2020-06-15 03:04:17 +0600

    )

--- core/views.py
+++ core/views.py
@@ -25,31 +25,38 @@ def home_view(request):
     global context
     markets = Area.objects.all().order_by('-updated_at')[:10]
     shops = Shop.objects.all().order_by('-updated_at')[:10]
-    items = Item.objects.all().order_by('-updated_at')[:10]
-    top = Item.objects.all().order_by(
-        '-updated_at').order_by('-created_at')[:10]
-    new = Item.objects.all().order_by(
-        '-updated_at').order_by('updated_at')[:10]
+    # items = Item.objects.all().order_by('-updated_at')[:10]
+    top = Item.objects.all().order_by('-created_at')[:10]
+    new = Item.objects.all().order_by('updated_at')[:10]
     # temp
-    latest = Item.objects.all().order_by(
-        '-updated_at').order_by('category')[:10]
+    latest = Item.objects.all().order_by('category')[:10]
 
     cats = Category.objects.all().order_by('-updated_at')
-
-    cats_nav = Category.objects.all().order_by(
-        '-updated_at').order_by('-updated_at')[:5]
+    
+    cats = random.sample(list(cats), len(cats))
+    cats_nav = cats[:5]
     cat_items = {}
+   
     for cat in cats:
         content = Item.objects.filter(category=cat)
+        try:
+            x = random.randint(6,len(content))
+            content = random.sample(list(content), 6)
+        except:
+            if (len(content)<6):
+                content = random.sample(list(content), len(content))
+            else:
+                content = random.sample(list(content), len(content))
         if cat.name not in cat_items.keys():
-            print("CAT NAME", cat.name)
-            cat_items[str(cat.name)] = content
-            print(content)
+            
+            if len(content)>0 :
+                cat_items[str(cat.name)] = content
+                
     # print(request.user.shop)
     # print(cat_items['cloth'])
     # slide1 = Slider.objects.all().order_by('-updated_at').first()
-    slider = Slider.objects.all().order_by('-updated_at')
-    context = {'items': items, 'shops': shops,
+    slider = Slider.objects.all().order_by('-updated_at')[:5]
+    context = { 'shops': shops,
                'markets': markets, 'cats': cats, 'cats_nav': cats_nav,
                'slider': slider,
                'cat_items': cat_items,
@@ -687,10 +694,12 @@ def category_view(request, slug):
     cats = Category.objects.all().order_by('-updated_at')
     cat_items = {}
     for ca in cats:
+
         content = Item.objects.filter(category=ca)
         if cat.name not in cat_items.keys():
             print("CAT NAME", ca.name)
-            cat_items[str(ca.name)] = content
+            if len(content) > 0:
+                cat_items[str(ca.name)] = content
             print(content)
     context = {
         'items': items,
