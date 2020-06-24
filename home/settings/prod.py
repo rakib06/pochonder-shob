68b6a6b6af752ce45636c40b2dbf4732928b8706
rakib06 <six.rakib@gmail.com>
2020-06-24 15:33:40 +0600

    )

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -61,7 +61,7 @@ if os.getcwd() == '/app':
 
     # Allow all host headers
     ALLOWED_HOSTS = ['pochonder-shob.herokuapp.com', 'pochonder-shob.com']
-    DEBUG = True
+    # DEBUG = True
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
