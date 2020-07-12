7d0ee21b34452254e034528a730895e39d5d1112
rakib06 <six.rakib@gmail.com>
2020-07-12 07:06:34 +0600

    view changed for search category 12 July

--- core/views.py
+++ core/views.py
@@ -985,12 +985,23 @@ def get_items_slug(request, slug):
             item_p = paginator.page(1)
         except EmptyPage:
             item_p = paginator.page(paginator.num_pages)
+        
+        # Get the index of the current page
+        index = item_p.number - 1  # edited to something easier without index
+        # This value is maximum index of your pages, so the last page - 1
+        max_index = len(paginator.page_range)
+        # You want a range of 7, so lets calculate where to slice the list
+        start_index = index - 3 if index >= 3 else 0
+        end_index = index + 3 if index <= max_index - 3 else max_index
+        # Get our new page range. In the latest versions of Django page_range returns 
+        # an iterator. Thus pass it to list, to make our slice possible again.
+        page_range = list(paginator.page_range)[start_index:end_index]      
 
         context.update ({'items': item_p, 'category': category,
                    'offer': offer, 'shop': shop,'root_cat': RootCat.objects.all(),
         'shops': Shop.objects.all(),
         'markets': Area.objects.all(),
-                   'cats_all': cats_all,})
+                   'cats_all': cats_all, 'page_range': page_range})
         print(items)
         # return render(request, 'a/main/items.html', context)
         return render(request, 'visitor/shop/getItems.html', context)
